953e091e27ed4fd20d6a9f6c1f5ec872a974931f
Renata Toth <renata.anna.toth@gmail.com>
2024-03-18 00:01:51 +0100

    Create tablet view for Portfolio page

--- src/components/Portfolio/PortfolioCard.js
+++ src/components/Portfolio/PortfolioCard.js
@@ -2,13 +2,19 @@ import classes from './PortfolioCard.module.scss';
 
 const PortfolioCard = ({projectData, showNumber}) => {
     return (
-        <div className={classes.card} style={{backgroundImage: `url(${require(`../../assets/portfolio/desktop/${projectData.imgUrl}`)}`}}>
+        <div className={classes.card}>
             <div className={classes.overlay}></div>
-            <div className={classes.textWrapper}>
+            <div className={classes.imgContainer}>
+                <picture>
+                    <source media="(max-width: 768px)" srcSet={require(`../../assets/portfolio/tablet/${projectData.imgUrl}`)}/>
+                    <img src={require(`../../assets/portfolio/desktop/${projectData.imgUrl}`)} alt={projectData.title}/>
+                </picture>
+            </div>
+            <div className={classes.textContainer}>
                 <h3 className={classes.projectTitle}>{projectData.title}</h3>
                 <p className={classes.projectDate}>{projectData.date}</p>
-                {showNumber && <span className={classes.projectNumber}>{projectData.id}</span>}
             </div>
+            {showNumber && <span className={classes.projectNumber}>{projectData.id}</span>}
         </div>
     );
 };
