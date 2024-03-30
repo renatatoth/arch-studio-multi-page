8cb41d1520ef3f107ba1c89b60a257fe2e1c8fda
Renata Toth <renata.anna.toth@gmail.com>
2024-03-30 18:25:15 +0100

    Create responsive hamburger menu for mobile view

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,19 +1,33 @@
 import {Link, NavLink} from "react-router-dom";
 import classes from'./Header.module.scss';
 import logo from '../../assets/logo.svg';
+import menu from '../../assets/icons/icon-hamburger.svg';
 
-const Header = () => {
+const Header = ({menuVisible, onToggleMenu, onCloseMenu}) => {
+
+
     return (
         <header className={classes.header}>
             <nav className={classes.nav}>
                 <div className={classes.logoContainer}>
-                    <Link to="/"><img src={logo} alt="Logo"/></Link>
+                    <Link to="/" onClick={onCloseMenu}><img src={logo} alt="Logo"/></Link>
                 </div>
-                <ul className={classes.navList}>
-                    <li className={classes.navItem}><NavLink to="/portfolio" className={({isActive}) => isActive ? `${classes.active}` : ''}>Portfolio</NavLink></li>
-                    <li className={classes.navItem}><NavLink to="/about" className={({isActive}) => isActive ? `${classes.active}` : ''}>About Us</NavLink></li>
-                    <li className={classes.navItem}><NavLink to="/contact" className={({isActive}) => isActive ? `${classes.active}` : ''}>Contact</NavLink></li>
-                </ul>
+                {menuVisible &&
+                    <ul className={classes.navList}>
+                        <li className={classes.navItem}>
+                            <NavLink to="/portfolio" onClick={onCloseMenu} className={({isActive}) => isActive ? `${classes.active}` : ''}>Portfolio</NavLink>
+                        </li>
+                        <li className={classes.navItem}>
+                            <NavLink to="/about" onClick={onCloseMenu} className={({isActive}) => isActive ? `${classes.active}` : ''}>About Us</NavLink>
+                        </li>
+                        <li className={classes.navItem}>
+                            <NavLink to="/contact" onClick={onCloseMenu} className={({isActive}) => isActive ? `${classes.active}` : ''}>Contact</NavLink>
+                        </li>
+                    </ul>
+                }
+                <div className={classes.mobileMenu} onClick={onToggleMenu}>
+                    <img src={menu} alt="Menu icon"/>
+                </div>
             </nav>
         </header>
     );
--- src/routes/RootLayout.js
+++ src/routes/RootLayout.js
@@ -1,13 +1,33 @@
+import {useEffect, useState} from "react";
 import {Outlet} from "react-router-dom";
 import Header from "../components/Layout/Header";
 import Footer from "../components/Layout/Footer";
 import ScrollToTop from "../helpers/ScrollToTop";
 import classes from './RootLayout.module.scss';
+import useWindowWidth from "../hooks/useWindowWidth";
 
 const RootLayout = () => {
+    const [menuVisible, setMenuVisible] = useState(false);
+    const viewportWidth = useWindowWidth();
+
+    useEffect(() => {
+        // show or hide menu when resizing window
+        if (viewportWidth <= 375) setMenuVisible(false);
+        else setMenuVisible(true);
+    }, [viewportWidth]);
+
+    const handleMenuToggle = () => {
+        setMenuVisible(vis => !vis);
+    };
+
+    const handleMenuClose = () => {
+        if (viewportWidth <= 375) setMenuVisible(false);
+    };
+
     return (
         <div className={classes.wrapper}>
-            <Header/>
+            {menuVisible && <div className={classes.modalOverlay}></div>}
+            <Header menuVisible={menuVisible} onToggleMenu={handleMenuToggle} onCloseMenu={handleMenuClose} />
             <main className={classes.main}>
                 <ScrollToTop/>
                 <Outlet/>
